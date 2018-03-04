65da9a6473e0e43eb4cbb32d6c39bd54b7b7779e
Dheeraj Bharti <dbharti2000@gmail.com>
2018-03-04 19:18:38 +0000

    Added log4j + added .gitignore

--- src/test/java/gumtree/cars/stepdefinitions/GeneralStepDefs.java
+++ src/test/java/gumtree/cars/stepdefinitions/GeneralStepDefs.java
@@ -4,12 +4,15 @@ import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import gumtree.cars.driver.DriverManager;
 import org.openqa.selenium.WebDriver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 
 public class GeneralStepDefs {
 
     public static WebDriver driver;
+    private static Logger LOG = LogManager.getLogger(GeneralStepDefs.class);
 
     @Before
     public void beforeScenario() throws IOException {
